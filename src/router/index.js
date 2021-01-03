c7d351342333a6ea3bfd1bfc14e0658f3c160cd8
refaat1297 <a.refaat1297@gmail.com>
2021-01-03 21:40:36 +0200

    add PageNotFound route

--- src/router/index.js
+++ src/router/index.js
@@ -2,28 +2,35 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+
+import PageNotFound from '../views/errors/404'
+
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '*',
+        component: PageNotFound
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
---
+++ src/views/errors/404.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <h1 class="alert alert-danger">page not found</h1>
+        <p class="alert alert-info">hello from the other side</p>
+        <router-link to="/" class="btn btn-info">Home</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "404"
+}
+</script>
+
+<style scoped>
+
+</style>
